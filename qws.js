36857796a25d62a7e8a163ba9525a636ba675181
WenSong Qi <qwstdx@gmail.com>
2023-05-24 00:13:23 +0800

    Create qws.js

---
+++ qws.js
@@ -0,0 +1,3 @@
+[Script]
+# > ChatGPT 解锁 Plus 订阅
+^https?:\/\/ios\.chat\.openai\.com\/backend-api\/(accounts\/check\/|models) url script-response-body https://raw.githubusercontent.com/yqc007/QuantumultX/master/ChatGPTPlusCrack.js
